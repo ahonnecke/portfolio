01e50c7aaa35241671c15e7fccc1e53fe789575c
Ashton Honnecke <ashton@pixelstub.com>
2024-04-24 11:58:05 -0600

    Card detail

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,6 +4,19 @@ import "./App.css";
 import myVideo from "../public/fire_table.mov";
 import quadLit from "../public/quad_lit.jpg";
 
+import LintingCard from "../public/linting_card.png";
+import BigWheelCard from "../public/big_wheel_card.png";
+import FireTableCard from "../public/fire_table_card.png";
+import ConsoloCard from "../public/consolo_card.png";
+import CvCard from "../public/cv_card.png";
+import DevopsCard from "../public/devops_card.png";
+import DockerCard from "../public/docker_card.png";
+import GithubCard from "../public/github_card.png";
+import HomeCard from "../public/home_card.jpg";
+import JollyBrancherCard from "../public/jolly_brancher_card.png";
+import QuadricycleCard from "../public/quadricycle_card.png";
+import SnifterCard from "../public/snifter_card.png";
+
 interface NavMap {
   [key: string]: {
     link: string;
@@ -11,112 +24,132 @@ interface NavMap {
     text: string;
     color: string;
     category: string;
-    detail_component: string;
+    detail: string;
   };
 }
 
 const navMap: NavMap = {
   big_wheel: {
     link: "https://drive.google.com/file/d/0B2sPu-smnJTpX2hCc2JLWTNSN3M/view?resourcekey=0-76YjSJXAvs0IlX1lH6tkmw",
-    image: "big_wheel_card.png",
+    image: BigWheelCard,
     text: "Big Wheel",
     color: "#FDDBA5",
     category: "Art",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   fire_table: {
     link: "/fire_table/",
-    image: "fire_table.png",
+    image: FireTableCard,
     text: "Fire Table",
     color: "#FF4820",
     category: "Art",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   cv: {
     link: "https://pixelstub.com/cv/ashton_honnecke_cv.pdf",
-    image: "cv_card.png",
+    image: CvCard,
     text: "CV PDF",
     color: "#FDF4EB",
     category: "Doc",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   quadricycle: {
     link: "/quadricycle/",
-    image: "quadricycle_card.png",
+    image: QuadricycleCard,
     text: "Quadricycle",
     color: "#5B92A6",
     category: "Art",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   cleaning_up: {
     link: "https://www.youtube.com/watch?v=cpseEHA_haA",
-    image: "docker_card.png",
+    image: DockerCard,
     text: "Containers",
     color: "#5CE6ED",
     category: "Speaking",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   linting: {
-    link: "https://github.com/ahonnecke/linting-talk",
-    image: "linting_card.png",
+    link: "linting/",
+    image: LintingCard,
     text: "Linting",
     color: "#EDCCB8",
     category: "Speaking",
-    detail_component: "placeholder_detail_component",
+    detail: Linting,
   },
   consolo: {
     link: "https://pypi.org/project/consolo/",
-    image: "consolo_card.png",
+    image: ConsoloCard,
     text: "Consolo",
     color: "#EA5C48",
     category: "FOSS",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   snifter: {
     link: "https://pypi.org/project/snifter/",
-    image: "snifter_card.png",
+    image: SnifterCard,
     text: "Snifter",
     color: "#FEA11C",
     category: "FOSS",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   jolly_brancher: {
     link: "https://pypi.org/project/jolly-brancher/",
-    image: "jolly_brancher_card.png",
+    image: JollyBrancherCard,
     text: "Jolly Brancher",
     color: "#9DC043",
     category: "FOSS",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   github: {
     link: "https://github.com/ahonnecke/",
-    image: "github_card.png",
+    image: GithubCard,
     text: "Github",
     color: "#CED1FC",
     category: "Link",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   home: {
     link: "/",
-    image: "home_card.jpg",
+    image: HomeCard,
     text: "Home",
     color: "#FDFDFF",
     category: "placeholder_category",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
   devops: {
     link: "/devops/",
-    image: "devops_card.jpg",
+    image: DevopsCard,
     text: "DevOps",
     color: "#E2725C",
     category: "Link",
-    detail_component: "placeholder_detail_component",
+    detail: "placeholder_detail",
   },
 };
 
 function App() {
   return (
     <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/big_wheel" element={<BigWheel />} />
+        <Route path="/fire_table" element={<FireTable />} />
+        <Route path="/cv" element={<CV />} />
+        <Route path="/quadricycle" element={<Quadricycle />} />
+        <Route path="/cleaning_up" element={<CleaningUp />} />
+        <Route path="/linting" element={<Linting />} />
+        <Route path="/consolo" element={<Consolo />} />
+        <Route path="/snifter" element={<Snifter />} />
+        <Route path="/jolly_brancher" element={<JollyBrancher />} />
+        <Route path="/github" element={<Github />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Main() {
+  return (
+    <>
       <div>
         <h1 className="fullname">
           Ashton Honnecke
@@ -143,26 +176,8 @@ function App() {
         <Tile tile="quadricycle" />
         <Tile tile="cv" />
       </div>
-
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/big_wheel" element={<BigWheel />} />
-        <Route path="/fire_table" element={<FireTable />} />
-        <Route path="/cv" element={<CV />} />
-        <Route path="/quadricycle" element={<Quadricycle />} />
-        <Route path="/cleaning_up" element={<CleaningUp />} />
-        <Route path="/linting" element={<Linting />} />
-        <Route path="/consolo" element={<Consolo />} />
-        <Route path="/snifter" element={<Snifter />} />
-        <Route path="/jolly_brancher" element={<JollyBrancher />} />
-        <Route path="/github" element={<Github />} />
-      </Routes>
-    </Router>
+    </>
   );
-}
-
-function Main() {
-  return <div></div>;
 }
 
 function Tile({ tile }: { tile: string }) {
@@ -177,6 +192,9 @@ function Tile({ tile }: { tile: string }) {
           </span>
           <span className="cardTitle" style={{ color: navData.color }}>
             {navData.text}
+          </span>
+          <span className="cardDetail" style={{ color: navData.color }}>
+            {navData.detail}
           </span>
         </span>
       </span>
@@ -231,15 +249,6 @@ function CleaningUp() {
   );
 }
 
-function Linting() {
-  return (
-    <span>
-      <span>The Value of Linting</span>
-      <p>Details on the value of linting...</p>
-    </span>
-  );
-}
-
 function Consolo() {
   return (
     <span>
@@ -272,6 +281,31 @@ function Github() {
     <span>
       <span>Github</span>
       <p>My social media links...</p>
+    </span>
+  );
+}
+
+function Linting() {
+  const navData = navMap["linting"];
+  return (
+    <span>
+      <span>The Value of Linting</span>
+      <img src={navData.image} alt={navData.text} />
+      <p>
+        I delivered a local talk on linting, which is now only{" "}
+        <a href="https://github.com/ahonnecke/linting">hosted on GitHub</a>
+        (the recoding was lost in the fervor of COVID). This talk, which I
+        authored and, delved deep into the world of linting, a crucial aspect of
+        programming that ensures code quality and readability. Linting, a type
+        of static code analysis, checks code for potential errors, bugs,
+        stylistic errors, and suspicious constructs. In this talk, I guided the
+        audience through the importance of linting, how to set it up, and how to
+        use it to improve code quality. The talk was beneficial for both
+        seasoned developers and beginners in coding, offering valuable insights
+        into the best practices of using linting tools in projects. This
+        experience enhanced my coding skills and deepened my understanding of
+        linting[1].
+      </p>
     </span>
   );
 }
